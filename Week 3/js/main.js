1530bf3cccf1f604686255392f68b9e4ff66b214
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-09-22 15:54:46 -0400

    second

--- Week 3/js/main.js
+++ Week 3/js/main.js
@@ -4,3 +4,13 @@ function opposite(number) {
   //your code here
   return -number
 }
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+// 
+// Your task is to write a function which returns the time since midnight
+// in milliseconds.
+
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  return h * 3600000 + m * 60000 + s * 1000
+}
