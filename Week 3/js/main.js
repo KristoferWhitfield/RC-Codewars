1e2c5cbf0b59b10f00e7dea9c993a60fbcc0c861
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-09-24 11:52:21 -0400

    last code war

--- Week 3/js/main.js
+++ Week 3/js/main.js
@@ -38,3 +38,16 @@ var stringToNumber = function(str){
   // put your code here
   return parseFloat(str)
 }
+
+
+// Your task is to create functionisDivideBy (or is_divide_by) to check if an integer
+//  number is divisible by each out of two arguments.
+
+function isDivideBy(number, a, b) {
+  // good luck
+  if(number % a === 0 && number % b === 0){
+    return true
+  }else {
+    return false
+  }
+}
