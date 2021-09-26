e982612b57352b476abd735689450e100c114663
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-09-25 20:39:49 -0400

    new week

---
+++ Week 4/js/main.js
@@ -0,0 +1,53 @@
+// Very simple, given a number, find its opposite.
+
+function opposite(number) {
+  //your code here
+  return -number
+}
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+//
+// Your task is to write a function which returns the time since midnight
+// in milliseconds.
+
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  return h * 3600000 + m * 60000 + s * 1000
+}
+
+// Create a function that takes a number as an argument and
+// returns a grade based on that number.
+function grader(score) {
+  if(score > 1 || score < .6){
+    return "F"
+  } else if(score >= .9){
+    return "A"
+  } else if(score >= .8){
+    return "B"
+  } else if(score >= .7){
+    return "C"
+  } else if(score >= .6){
+    return "D"
+  }
+}
+
+// We need a function that can transform a string into a number.
+//  What ways of achieving this do you know?
+
+var stringToNumber = function(str){
+  // put your code here
+  return parseFloat(str)
+}
+
+
+// Your task is to create functionisDivideBy (or is_divide_by) to check if an integer
+//  number is divisible by each out of two arguments.
+
+function isDivideBy(number, a, b) {
+  // good luck
+  if(number % a === 0 && number % b === 0){
+    return true
+  }else {
+    return false
+  }
+}
