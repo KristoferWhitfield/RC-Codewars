2ea2fc0a32536e61b9a41b3a6ca60ce4fd8f8808
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-09-29 20:32:00 -0400

    day 3

--- Week 4/js/main.js
+++ Week 4/js/main.js
@@ -27,3 +27,20 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return false
   }
 };
+
+// Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps
+// the animals do not have the right tails. To help her, you must correct the broken
+// function to make sure that the second argument (tail), is the same as the last
+// letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+function correctTail(body, tail){
+
+  sub = body.substr(body.length-(tail.length))
+
+  if (sub === tail){
+    return true
+  }
+  else {
+    return false
+    }
+}
