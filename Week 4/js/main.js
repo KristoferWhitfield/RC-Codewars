bce895a60e4ed4d270b61751f497e703d853698c
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-01 09:29:47 -0400

    day 5

--- Week 4/js/main.js
+++ Week 4/js/main.js
@@ -49,3 +49,15 @@ function correctTail(body, tail){
 
 var a = "123";
 a.toString()
+
+// This kata is about multiplying a given number by eight if it is
+// an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    // your code........
+  if(number % 2 === 0){
+    return number * 8
+  } else {
+    return number * 9
+  }
+}
