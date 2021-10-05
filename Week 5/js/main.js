4c1c759cf28607aa497b298e1ae3ebab085929e1
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-05 18:18:24 -0400

    day 2

--- Week 5/js/main.js
+++ Week 5/js/main.js
@@ -3,3 +3,15 @@
 var a = "code";
 var b = "wa.rs";
 var name = a + b;
+
+// This function takes two numbers as parameters, the first number being
+// the coefficient, and the second number being the exponent.
+// Your function should multiply the two numbers, and then subtract 1 from the
+// exponent. Then, it has to print out an expression (like 28x^7). "^1" should
+// not be truncated when exponent = 2.
+
+function derive(coefficient,exponent) {
+  let a = coefficient * exponent
+  let b = exponent - 1
+  return a.toString() + "x^" + b.toString()
+}
