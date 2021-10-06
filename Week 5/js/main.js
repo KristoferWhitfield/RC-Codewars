3a5c39789dcc1b5df9be5ac57ccf44d768063b8b
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-06 18:25:14 -0400

    day 3

--- Week 5/js/main.js
+++ Week 5/js/main.js
@@ -15,3 +15,17 @@ function derive(coefficient,exponent) {
   let b = exponent - 1
   return a.toString() + "x^" + b.toString()
 }
+
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+// Numbers may be positive or negative, integers or floats.
+
+function testEven(n) {
+    //Your awesome code here!
+  if(n % 2 == 0){
+    return true
+  } else {
+    return false
+  }
+}
