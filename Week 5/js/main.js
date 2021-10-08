fec1c439690aafc2a071f4f441528438f44dc489
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-08 12:18:34 -0400

    day 5

--- Week 5/js/main.js
+++ Week 5/js/main.js
@@ -29,3 +29,21 @@ function testEven(n) {
     return false
   }
 }
+
+// Given an integral number of watermelons w (1 ≤ w ≤ 100; 1 ≤ w in Haskell),
+//  check whether Pete and Billy can divide the melons so that each of them gets
+//   an even amount.
+
+function divide(weight){
+  if(weight > 2 && weight % 2  === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+function greet(){
+    return 'hello world!'
+}
