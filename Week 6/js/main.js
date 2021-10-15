c87269cdb42c02320d9f31476e0cf64ac6cda0a5
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-15 18:24:59 -0400

    day 4

--- Week 6/js/main.js
+++ Week 6/js/main.js
@@ -44,3 +44,12 @@ function hello(name) {
     return `Hello, ${name[0].toUpperCase()}${name.substring(1,).toLowerCase()}!`;
   }
 }
+
+// It's pretty straightforward. Your goal is to create a function that removes the
+// first and last characters of a string. You're given one parameter, the original
+// string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+ //You got this!
+ return str.slice(1, str.length - 1)
+};
