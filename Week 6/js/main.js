a2f9825200eae77a497891f6fb13fb197661122a
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-14 20:20:32 -0400

    day 3

--- Week 6/js/main.js
+++ Week 6/js/main.js
@@ -29,6 +29,18 @@ function whatday(num) {
   }else {
     return "Wrong, please enter a number between 1 and 7"
   }
+}
 
+// Define a method hello that returns "Hello, Name!" to a given name, or says
+// Hello, World! if name is not given (or passed as an empty String).
+//
+// Assuming that name is a String and it checks for user typos to return a
+// name with a first capital letter (Xxxx).
 
+function hello(name) {
+  if (typeof name === "undefined" || name.length === 0) {
+    return `Hello, World!`;
+  } else {
+    return `Hello, ${name[0].toUpperCase()}${name.substring(1,).toLowerCase()}!`;
+  }
 }
