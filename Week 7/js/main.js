ef671448ee574f1aa96a1b68275ed8b8d7cbb28f
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-22 11:29:50 -0400

    day 4

--- Week 7/js/main.js
+++ Week 7/js/main.js
@@ -12,8 +12,8 @@ function noSpace(x){
   return x.split(" ").reduce((a, c) => a + c)
 }
 
-Consider an array/list of sheep where some sheep may be missing from their place. 
-We need a function that counts the number of sheep present in the array (true means present).
+// Consider an array/list of sheep where some sheep may be missing from their place.
+// We need a function that counts the number of sheep present in the array (true means present).
 
 function countSheeps(arrayOfSheep) {
   // TODO May the force be with you
@@ -22,4 +22,15 @@ function countSheeps(arrayOfSheep) {
         if(arrayOfSheep[i]) count += 1
     }
     return count
+}
+
+// It's the academic year's end, fateful moment of your school report. The averages 
+// must be calculated. All the students come to you and entreat you to calculate
+// their average for them. Easy ! You just need to write a script.
+//
+// Return the average of the given array rounded down to its nearest integer.
+
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
+  return Math.floor(marks.reduce((x, y) => x + y) / marks.length)
 }
