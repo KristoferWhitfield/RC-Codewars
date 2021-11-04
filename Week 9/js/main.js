7a25b56787c03c67d16b1eea35ca9c97dcdd0d28
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-11-04 19:40:37 -0400

    day 4

--- Week 9/js/main.js
+++ Week 9/js/main.js
@@ -25,3 +25,15 @@ var summation = function (num) {
     }
     return total;
 }
+
+// Write a function to convert a name into initials. This kata strictly
+// takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+function abbrevName(name){
+ var separate = name.split(" ");
+    var abbrev = (separate[0].charAt(0) + "." + separate[1].charAt(0)).toUpperCase();
+    return abbrev;
+    // code away
+
+}
