12eed691cf31153dfbb3ac75107c12c46b42b012
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-11-03 21:15:19 -0400

    day 3

--- Week 9/js/main.js
+++ Week 9/js/main.js
@@ -5,7 +5,7 @@ function removeEveryOther(arr){
   //your code here
   return arr.filter((a,i) => i%2 == 0)
 }
-// 
+//
 // Given an array of integers as strings and numbers, return the sum of the array
 //  values as if all were numbers.
 //
@@ -13,4 +13,15 @@ function removeEveryOther(arr){
 
 function sumMix(x){
  return x.map(a => +a).reduce((a, b) => a + b);
+}
+
+// Write a program that finds the summation of every number from 1 to num.
+// The number will always be a positive integer greater than 0.
+
+var summation = function (num) {
+   var total = 0;
+    for(var i = 1; i <= num; i++){
+      total += i;
+    }
+    return total;
 }
