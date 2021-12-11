e80290e3627e9dcc1e1c39f539aa69535965245b
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-12-10 19:23:22 -0500

    end of week

--- Week 13/js/main.js
+++ Week 13/js/main.js
@@ -213,11 +213,11 @@ twoSums([2, 5, 7, 9], 7)
 
 
 
-Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
-The output should be two capital letters with a dot separating them.
+//The output should be two capital letters with a dot separating them.
 
-It should look like this:
+// should look like this:
 
 
 function abbrevName(name){
