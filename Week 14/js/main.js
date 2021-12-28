8355bf9cc4bb1ca8f84c14660c291512440d6d21
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-12-27 20:47:44 -0500

    pratice

--- Week 14/js/main.js
+++ Week 14/js/main.js
@@ -6,3 +6,9 @@
  function smash (words) {
    return words.join(" ")
 }
+
+// Create a function called shortcut to remove all the lowercase vowels in a given string.
+
+function shortcut(string){
+  return string.replace(/[aeiou]/g, "")
+}
