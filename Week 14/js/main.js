6477faf170aaf0229753dd486f2f45b0906e9d82
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-12-28 20:55:59 -0500

    pratice

--- Week 14/js/main.js
+++ Week 14/js/main.js
@@ -12,3 +12,10 @@
 function shortcut(string){
   return string.replace(/[aeiou]/g, "")
 }
+
+// You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+
+function otherAngle(a, b) {
+  return 180-(a+b)
+}
