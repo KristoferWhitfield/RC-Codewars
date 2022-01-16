6d2253a5da9a958fde5b33e9ee5da99455d9c0e0
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2022-01-16 11:30:49 -0500

    pratice

--- Week 16/js/main.js
+++ Week 16/js/main.js
@@ -1,6 +1,22 @@
-Given a non-empty array of integers, return the result of multiplying the
-values together in order.
+// Given a non-empty array of integers, return the result of multiplying the
+// values together in order.
 
 function grow(x){
   return x.reduce((a, b) => a*b )
+}
+
+// Write a function named setAlarm which receives two parameters. The first
+// parameter, employed, is true whenever you are employed and the second parameter,
+// vacation is true whenever you are on vacation.
+// The function should return true if you are employed and not on vacation
+//  (because these are the circumstances under which you need to set an alarm).
+//   It should return false otherwise.
+
+function setAlarm(employed, vacation){
+
+    if (employed == true && vacation == false){
+      return true
+    } else {
+      return false
+    }
 }
