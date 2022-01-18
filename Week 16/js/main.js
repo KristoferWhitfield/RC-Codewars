066cdf23b777da4369c86ea53ad72cb34fe77e32
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2022-01-17 22:04:06 -0500

    pratice

--- Week 16/js/main.js
+++ Week 16/js/main.js
@@ -20,3 +20,20 @@ function setAlarm(employed, vacation){
       return false
     }
 }
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+  // your code here
+
+  for(let i = 0; i < haystack.length; i++){
+
+    if(haystack[i] === "needle"){
+      return "found the needle at position " + i
+    }
+  }
+}
