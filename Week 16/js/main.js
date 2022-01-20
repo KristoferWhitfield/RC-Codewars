1d1ae298225cd5d6578aa978e30f8980e9c1c560
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2022-01-19 22:45:45 -0500

    pratice

--- Week 16/js/main.js
+++ Week 16/js/main.js
@@ -37,3 +37,13 @@ function findNeedle(haystack) {
     }
   }
 }
+
+// Given a set of numbers, return the additive inverse of each. Each positive
+// becomes negatives, and the negatives become positives.
+
+function invert(array) {
+
+
+  return array.map(x => x * -1)
+
+}
