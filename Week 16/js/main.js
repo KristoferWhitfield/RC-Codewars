70df213e8e610ff3b7f7c85e22f7613750b9462f
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2022-01-23 21:16:31 -0500

    pratice

--- Week 16/js/main.js
+++ Week 16/js/main.js
@@ -63,3 +63,16 @@ function areYouPlayingBanjo(name) {
   }
 
 }
+
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the
+// bug in his unfinished for loop!f
+
+function createArray(number){
+  var newArray = [];
+
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
