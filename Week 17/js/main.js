3214e7c31a697632ba2d831e176da906e4f67e1f
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2022-02-01 16:29:34 -0500

    pratice

--- Week 17/js/main.js
+++ Week 17/js/main.js
@@ -43,3 +43,21 @@ function combat(health, damage) {
     return health - damage
   }
 }
+
+This is the first step to understanding FizzBuzz.
+
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+
+Your expected output is an array of positive integers from 1 to n (inclusive).
+
+Your job is to write an algorithm that gets you from the input to the output.
+
+function preFizz(n) {
+
+  let array = []
+
+  for(let i = 1; i <= n; i++){
+    array.push(i)
+  }
+  return array
+}
