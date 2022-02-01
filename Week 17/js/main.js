90270c97e4fc4809600fe84e4e96c5ab91be98e2
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2022-01-31 20:17:11 -0500

    pratice

--- Week 17/js/main.js
+++ Week 17/js/main.js
@@ -30,3 +30,16 @@ function checkAlive (health) {
     return true
   }
 }
+
+Create a combat function that takes the player's current health and the amount
+of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+  // Write your code here
+
+  if(health - damage <= 0){
+    return 0
+  }else {
+    return health - damage
+  }
+}
